759a62064293a17795a28f26e0eb90ec102d4d97
Gargair <Gargair@users.noreply.github.com>
2024-12-06 18:30:42 +0100

    Improve Performance Day 06

--- Day06_Solution.cs
+++ Day06_Solution.cs
@@ -19,48 +19,7 @@ namespace AdventOfCode
             {
                 throw new InvalidOperationException("You have to call LoadData() before Part1()");
             }
-            List<long> positionsVisited = [];
-            int[] current = GrabStart(block);
-            long blockWidth = block[0].Length;
-            int deltaX = -1;
-            int deltaY = 0;
-            while (current[0] >= 0 && current[1] >= 0 && current[0] < block.Length && current[1] < blockWidth)
-            {
-                long posNum = current[0] * blockWidth + current[1];
-                if (!positionsVisited.Contains(posNum))
-                {
-                    positionsVisited.Add(posNum);
-                }
-                int[] next = [current[0] + deltaX, current[1] + deltaY];
-                if (next[0] >= 0 && next[1] >= 0 && next[0] < block.Length && next[1] < blockWidth)
-                {
-                    if (block[next[0]][next[1]] == '#')
-                    {
-                        if (deltaX == -1 && deltaY == 0)
-                        {
-                            deltaX = 0;
-                            deltaY = 1;
-                        }
-                        else if (deltaX == 0 && deltaY == 1)
-                        {
-                            deltaX = 1;
-                            deltaY = 0;
-                        }
-                        else if (deltaX == 1 && deltaY == 0)
-                        {
-                            deltaX = 0;
-                            deltaY = -1;
-                        }
-                        else if (deltaX == 0 && deltaY == -1)
-                        {
-                            deltaX = -1;
-                            deltaY = 0;
-                        }
-                        continue;
-                    }
-                }
-                current = next;
-            }
+            DeterminePathAndLoop(block, false, (current, blockWidth, deltaX, deltaY) => current[0] * blockWidth + current[1], out List<long> positionsVisited);
             return positionsVisited.Count;
         }
 
@@ -70,21 +29,24 @@ namespace AdventOfCode
             {
                 throw new InvalidOperationException("You have to call LoadData() before Part1()");
             }
-            // Bad Performance. Take path from part 1 into account to check for valid blocker
+            // Take actual path
+            DeterminePathAndLoop(block, false, (current, blockWidth, deltaX, deltaY) => current[0] * blockWidth + current[1], out List<long> positionsVisited);
+            long blockWidth = block[0].Length;
             long count = 0;
-            for (int i = 0; i < block.Length; i++)
+            foreach (long position in positionsVisited)
             {
-                for (int j = 0; j < block[i].Length; j++)
+                // Check for each position on path if blocker there would create a loop.
+                // All positions not on initial path have no impact on path taken.
+                int j = (int)(position % blockWidth);
+                int i = (int)(position / blockWidth);
+                if (block[i][j] == '.')
                 {
-                    if (block[i][j] == '.')
+                    block[i][j] = '#';
+                    if (DeterminePathAndLoop(block, true, (current, blockWidth, deltaX, deltaY) => (current[0] * blockWidth + current[1]) * 100 + deltaX * 10 + deltaY, out List<long> _))
                     {
-                        block[i][j] = '#';
-                        if (RunsInLoop(block))
-                        {
-                            count++;
-                        }
-                        block[i][j] = '.';
+                        count++;
                     }
+                    block[i][j] = '.';
                 }
             }
             return count;
@@ -105,21 +67,21 @@ namespace AdventOfCode
             return [0, 0];
         }
 
-        private static bool RunsInLoop(char[][] block)
+        private static bool DeterminePathAndLoop(char[][] block, bool stopOnVisitedPosition, Func<int[], long, int, int, long> posNumCalc, out List<long> positionsVisited)
         {
-            List<long> positionsVisited = [];
+            positionsVisited = [];
             int[] current = GrabStart(block);
             long blockWidth = block[0].Length;
             int deltaX = -1;
             int deltaY = 0;
             while (current[0] >= 0 && current[1] >= 0 && current[0] < block.Length && current[1] < blockWidth)
             {
-                long posNum = (current[0] * blockWidth + current[1]) * 100 + deltaX * 10 + deltaY;
+                long posNum = posNumCalc(current, blockWidth, deltaX, deltaY);
                 if (!positionsVisited.Contains(posNum))
                 {
                     positionsVisited.Add(posNum);
                 }
-                else
+                else if (stopOnVisitedPosition)
                 {
                     return true;
                 }
