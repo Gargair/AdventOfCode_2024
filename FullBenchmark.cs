a2cbae6458bcbd1a9bdf8148906268d361e3f5f1
Gargair <Gargair@users.noreply.github.com>
2025-01-06 21:03:38 +0100

    Add Benchmarking capabilities

---
+++ FullBenchmark.cs
@@ -0,0 +1,55 @@
+﻿using AdventOfCode.Helper;
+using BenchmarkDotNet.Attributes;
+
+namespace AdventOfCode
+{
+    public class FullBenchmark
+    {
+        [ParamsAllValues]
+        public Parts PartToBenchmark { get; set; }
+
+        public static IEnumerable<IDaySolution> SolutionsToBenchmark()
+        {
+            yield return new Day01_Solution();
+            yield return new Day02_Solution();
+            yield return new Day03_Solution();
+            yield return new Day04_Solution();
+            yield return new Day05_Solution();
+            yield return new Day06_Solution();
+            yield return new Day07_Solution();
+            //yield return new Day08_Solution();
+            //yield return new Day09_Solution();
+            //yield return new Day10_Solution();
+            //yield return new Day11_Solution();
+            //yield return new Day12_Solution();
+            //yield return new Day13_Solution();
+            //yield return new Day14_Solution();
+            //yield return new Day15_Solution();
+            //yield return new Day16_Solution();
+            //yield return new Day17_Solution();
+            //yield return new Day18_Solution();
+            //yield return new Day19_Solution();
+        }
+
+        [Benchmark]
+        [ArgumentsSource(nameof(SolutionsToBenchmark), Priority = -1)]
+        public void BenchmarkSolutions(IDaySolution Solution)
+        {
+            if ((PartToBenchmark & Parts.Part1) != 0)
+            {
+                string result = Solution.Part1("Input");
+            }
+
+            if ((PartToBenchmark & Parts.Part2) != 0)
+            {
+                string result = Solution.Part2("Input");
+            }
+        }
+
+        public enum Parts
+        {
+            Part1 = 1,
+            Part2 = 2
+        }
+    }
+}
--- Summary.cs
+++ Summary.cs
@@ -1,4 +1,5 @@
 ﻿using AdventOfCode.Helper;
+using BenchmarkDotNet.Running;
 using System.Diagnostics;
 
 namespace AdventOfCode
@@ -7,60 +8,48 @@ namespace AdventOfCode
     {
         public static void Main(string[] _args)
         {
+            DoFullBenchmark();
+            FullResults();
+        }
+
+        private static void DoFullBenchmark()
+        {
+            BenchmarkRunner.Run<FullBenchmark>();
+        }
+
+        private static void FullResults()
+        {
             Stopwatch timer = new();
             timer.Start();
-            StartSolution(new Day01_Solution());
-            StartSolution(new Day02_Solution());
-            StartSolution(new Day03_Solution());
-            StartSolution(new Day04_Solution());
-            StartSolution(new Day05_Solution());
-            StartSolution(new Day06_Solution());
-            StartSolution(new Day07_Solution());
-            StartSolution(new Day08_Solution());
-            StartSolution(new Day09_Solution());
-            StartSolution(new Day10_Solution());
-            StartSolution(new Day11_Solution());
-            StartSolution(new Day12_Solution());
-            StartSolution(new Day13_Solution());
-            StartSolution(new Day14_Solution());
-            StartSolution(new Day15_Solution());
-            StartSolution(new Day16_Solution());
-            StartSolution(new Day17_Solution());
-            StartSolution(new Day18_Solution());
-            StartSolution(new Day19_Solution());
-
+            foreach (IDaySolution solution in FullBenchmark.SolutionsToBenchmark())
+            {
+                StartSolution(solution);
+            }
             timer.Stop();
             Console.WriteLine($"[{DateTime.Now}]: Total: {timer.Elapsed}");
         }
 
-        private static void StartSolution<T, O>(IDaySolution<T, O> solution, string inputPath = "Input", Parts parts = Parts.Part1 | Parts.Part2)
+        private static void StartSolution(IDaySolution solution, string inputPath = "Input", Parts parts = Parts.Part1 | Parts.Part2)
         {
             string solutionName = solution.GetType().Name;
             Console.WriteLine($"[{DateTime.Now}]: {solutionName}");
-            Stopwatch timer = new();
 
             if ((parts & Parts.Part1) != 0)
             {
-                timer.Restart();
-                T input = solution.LoadData(inputPath);
-                O result = solution.Part1(input);
-                timer.Stop();
-                Console.WriteLine($"[{DateTime.Now}]: {solutionName} - Part 01: {timer.Elapsed}\t{result}");
+                string result = solution.Part1(inputPath);
+                Console.WriteLine($"[{DateTime.Now}]: {solutionName} - Part 01: \t{result}");
             }
 
             if ((parts & Parts.Part2) != 0)
             {
-                timer.Restart();
-                T input = solution.LoadData(inputPath);
-                O result = solution.Part2(input);
-                timer.Stop();
-                Console.WriteLine($"[{DateTime.Now}]: {solutionName} - Part 02: {timer.Elapsed}\t{result}");
+                string result = solution.Part2(inputPath);
+                Console.WriteLine($"[{DateTime.Now}]: {solutionName} - Part 02: \t{result}");
             }
 
             Console.WriteLine();
         }
 
-        private enum Parts
+        public enum Parts
         {
             Part1 = 1,
             Part2 = 2
